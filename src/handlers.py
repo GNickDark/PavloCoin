514076da760ebfce7bbaf4afdd4fb18886a7ea28
SickMan <132779342+GNickDark@users.noreply.github.com>
2025-02-21 23:31:55 +0300

    Update handlers.py - Introduction visual adjustments

--- src/handlers.py
+++ src/handlers.py
@@ -1,13 +1,26 @@
 from aiogram import Bot, Router
-from aiogram.types import InlineKeyboardButton,  InlineKeyboardMarkup, MenuButtonWebApp, Message, WebAppInfo
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, MenuButtonWebApp, Message, WebAppInfo
+from aiogram.filters import CommandStart
 
 router = Router()
 
-@router.message()
+@router.message(CommandStart())
 async def command_start(msg: Message, bot: Bot, base_url: str):
     await bot.set_chat_menu_button(
         chat_id=msg.chat.id,
         menu_button=MenuButtonWebApp(text="Start Tapping!", web_app=WebAppInfo(url=f"{base_url}/index")),
     )
     
-    await msg.answer("Hi and welcome to PavloCoin😎\nIt is time to tap!👆 Tap anytime and anywhere!⌚ Become the most powerful pavlik, PavloCoin tapper!😈", reply_markup=InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Open PavloCoin", web_app=WebAppInfo(url=f"{base_url}/index"))]]))
+    await msg.answer(
+        "Hi and welcome to PavloCoin😎\n"
+        "It is time to tap!👆 Tap anytime and anywhere!⌚\n"
+        "Become the most powerful pavlik, PavloCoin tapper!😈",
+        reply_markup=InlineKeyboardMarkup(
+            inline_keyboard=[[
+                InlineKeyboardButton(
+                    text="Open PavloCoin",
+                    web_app=WebAppInfo(url=f"{base_url}/index")
+                )
+            ]]
+        )
+    )
